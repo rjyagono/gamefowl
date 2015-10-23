a571909eb2bc1955c6c9a566a9d435ddbe45009a
jyagono <jude@mdoffice.in>
2015-10-23 20:24:18 +0800

    added logo & image

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,8 @@
 class Contact < ActiveRecord::Base
   attr_accessible :email
-	
+
 	validates :email, presence: true
+	validates :email, :format => {  :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, 
+																:if => Proc.new { |o|  o.email }
+														 }
 end
