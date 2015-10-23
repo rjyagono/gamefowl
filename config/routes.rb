b3daeaaffca4e0fde3076b67a3c8cb52f053747a
jyagono <jude@mdoffice.in>
2015-10-23 15:20:03 +0800

    change landing page

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ GameFowl::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'fowls#index'
+   root :to => 'contacts#index'
 
   # See how all your routes lay out with "rake routes"
 
