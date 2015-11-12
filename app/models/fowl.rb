cee15daf09966bc84cadda7609f1329ea7b9a54f
ransam <ransam@ransam-virtual-machine.(none)>
2015-11-12 18:52:03 +0800

    video fixes

--- app/models/fowl.rb
+++ app/models/fowl.rb
@@ -12,13 +12,12 @@ class Fowl < ActiveRecord::Base
 	
 	# has_attached_file :image, styles: { :thumb => "600x600#", :medium => "300x300#", :small => "160x160#" }
 	has_attached_file :image, styles: { :thumb => "400x400#", :medium => "200x200#", :small => "100x100#" }
-	# has_attached_file :video, styles: { :thumb => "300x300#", :medium => "200x200#", :small => "100x100#" }
+	has_attached_file :video
 
-
-	has_attached_file :video, :styles => {
-    	:medium => { :geometry => "640x480", :format => 'mp4' },
-    	:thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 }
-	}
+	# has_attached_file :video, :styles => {
+ #    	:medium => { :geometry => "640x480", :format => 'mp4' },
+ #    	:thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10 }
+	# }
 	
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/ 
 	validates_attachment_content_type :video, content_type: /\Avideo\/.*\Z/
