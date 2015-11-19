2408c1cd5ee5d7c403d0cd4907a509944968e6d6
ransam <ransam@ransam-virtual-machine.(none)>
2015-11-19 19:39:09 +0800

    sdfsdf

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
-SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook google_bookmark tumblr)
-end
+# SocialShareButton.configure do |config|
+#   config.allow_sites = %w(twitter facebook google_bookmark tumblr)
+# end
