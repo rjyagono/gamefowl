f6ceabc6a0aa683c1010a7420f20ded6b6bee609
ransam <ransam@ransam-virtual-machine.(none)>
2015-11-30 16:17:29 +0800

    omniauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ GameFowl::Application.routes.draw do
 
 
 
-  devise_for :users, :controllers => { :registrations => "registrations"}
+  devise_for :users, :controllers => { :registrations => "registrations", omniauth_callbacks: "omniauth_callbacks" }
 
 	resources :fowls do
 		member do
