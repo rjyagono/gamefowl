0a35333308014e883eb7a073593412352d5192a2
ransam <ransam@ransam-virtual-machine.(none)>
2015-12-02 15:54:43 +0800

    remove current user from callback

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,10 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 	skip_before_filter :authenticate_user!
+
 	def all
 		p env["omniauth.auth"]
-		user = User.from_omniauth(env["omniauth.auth"], current_user)
+		user = User.from_omniauth(env["omniauth.auth"])
 		if user.persisted?
 			flash[:notice] = "You are in..!!! Go to edit profile to see the status for the accounts"
 			sign_in_and_redirect(user)
@@ -21,11 +22,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
 	alias_method :facebook, :all
-	alias_method :twitter, :all
-	alias_method :linkedin, :all
-	alias_method :github, :all
-	alias_method :passthru, :all
-	alias_method :google_oauth2, :all
 
 
 end
