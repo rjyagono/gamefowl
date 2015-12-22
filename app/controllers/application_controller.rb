2173c592bd7d9ab39ddc164964d240af3e950632
jyagono <jude@mdoffice.in>
2015-12-22 21:46:57 +0800

    added subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :get_fowls
 
+private
+  def get_fowls
+  	user = User.find_by_name(request.subdomain)
+ 
+    if user.present?
+      @fowls = user.fowls
+    elsif request.subdomain != 'www'
+      redirect_to root_url(subdomain: 'www')
+    end
+  end
   # private
 
   # def current_user
