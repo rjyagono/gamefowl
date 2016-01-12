60066651782b4e7f9c4227de7132f6b89fee1100
ransam <ransam@ransam-virtual-machine.(none)>
2016-01-12 18:14:26 +0800

    ttttttttttt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,10 @@ class ApplicationController < ActionController::Base
 def after_sign_in_path_for(resource)
   sign_in_url = new_user_session_url
   if request.referer == sign_in_url
-    super
+    root_url(subdomain: current_user.store_name)
   else
-    stored_location_for(resource) || request.referer || root_url(subdomain: current_user.store_name)
+    #stored_location_for(resource) || request.referer ||
+    super
   end
 end
 
