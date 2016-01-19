61cc373e2fea1181c6ba19e7fca86b77c09e17b9
ransam <ransam@ransam-virtual-machine.(none)>
2016-01-19 20:14:37 +0800

    adding commit rotation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,6 @@ GameFowl::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.log", 'daily')
+  config.eager_load = true
 end
